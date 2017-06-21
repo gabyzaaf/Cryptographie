83b1fbf4c782ffb08ab024c7e2e9bfd5dce55b30
soleng <sola-prog@live.fr>
2017-06-21 10:59:25 +0200

    init project

---
+++ src/main/java/com/crypto/securityToolBox/com/crypto/securityTool/App.java
@@ -0,0 +1,13 @@
+package com.crypto.securityToolBox.com.crypto.securityTool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/crypto/securityToolBox/com/crypto/securityTool/AppTest.java
@@ -0,0 +1,10 @@
+package com.crypto.securityToolBox.com.crypto.securityTool;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase{
+	
+}
