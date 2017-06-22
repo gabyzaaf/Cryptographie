cece47945b00790409d42187e93362c4fe37b6c0
soleng <sola-prog@live.fr>
2017-06-22 18:17:45 +0200

    change solution to Cipher

--- src/main/java/com/crypto/securityToolBox/com/crypto/securityTool/RSA.java
+++ src/main/java/com/crypto/securityToolBox/com/crypto/securityTool/RSA.java
@@ -1,115 +1,67 @@
-package com.esgi.API;
+package com.crypto.securityToolBox.com.crypto.securityTool;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
 
 public class RSA {
-    private BigInteger n, privateK, publicK;
+    private KeyPair keyPair;
+    private KeyPair keyPairCli;
 
-    private int bitlen = 1024;
-
-    /**
-     * Create an instance that can encrypt using someone elses public key.
-     */
-    public RSA(BigInteger newn, BigInteger newe) {
-        n = newn;
-        publicK = newe;
-    }
-
-    public RSA(BigInteger newn, BigInteger newe, BigInteger newd) {
-        this(newn, newe);
-        privateK = newd;
-    }
-
-    /**
-     * Create an instance that can both encrypt and decrypt.
-     */
-    public RSA(int bits) {
-        bitlen = bits;
-        generateKeys();
-    }
-
-    /**
-     * Encrypt the given plaintext message.
-     */
-    public synchronized String encrypt(String message) {
-        return (new BigInteger(message.getBytes())).modPow(publicK, n).toString();
-    }
-
-    /**
-     * Encrypt the given plaintext message.
-     */
-    public synchronized BigInteger encrypt(BigInteger message) {
-        return message.modPow(publicK, n);
-    }
-
-    /**
-     * Decrypt the given ciphertext message.
-     */
-    public synchronized String decrypt(String message) {
-        return new String((new BigInteger(message)).modPow(privateK, n).toByteArray());
-    }
-
-    /**
-     * Decrypt the given ciphertext message.
-     */
-    public synchronized BigInteger decrypt(BigInteger message) {
-        return message.modPow(privateK, n);
-    }
-
-
-    /**
-     * Encrypt to transmission Serve -> Cli
-     * @param message String
-     * @return String
-     */
-    public synchronized String encryptP(String message) {
-        return (new BigInteger(message.getBytes())).modPow(privateK, n).toString();
-    }
-    /**
-     * Decrypt to transmission Cli -> Serve
-     * @param message String
-     * @return String
-     */
-    public synchronized String decryptP(String message) {
-        return new String((new BigInteger(message)).modPow(publicK, n).toByteArray());
-    }
-
-    /**
-     * Generate a new public and private key set.
-     */
-    public synchronized void generateKeys() {
-        SecureRandom r = new SecureRandom();
-        BigInteger p = new BigInteger(bitlen / 2, 100, r);
-        BigInteger q = new BigInteger(bitlen / 2, 100, r);
-        n = p.multiply(q);
-        BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q
-                .subtract(BigInteger.ONE));
-        publicK = new BigInteger("3");
-        while (m.gcd(publicK).intValue() > 1) {
-            publicK = publicK.add(new BigInteger("2"));
+    RSA() {
+        try {
+            keyPair = buildKeyPair();
+            keyPairCli = buildKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
-        privateK = publicK.modInverse(m);
+    }
+
+    public PublicKey getPublicKey() {
+        return keyPair.getPublic();
+    }
+
+    public PrivateKey getPrivateKeyToCli() {
+        return keyPairCli.getPrivate();
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        final int keySize = 1024;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(keySize);
+        return keyPairGenerator.genKeyPair();
     }
 
     /**
-     * Return the modulus.
+     *
+     * @param privateKey PrivateKey
+     * @param message String
+     * @return byte[]
+     * @throws Exception
      */
-    public synchronized BigInteger getN() {
-        return n;
+    public static byte[] encrypt(PrivateKey privateKey, String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+        return cipher.doFinal(message.getBytes());
     }
 
     /**
-     * Return the public key.
+     *
+     * @param publicKey PublicKey
+     * @param encrypted byte[]
+     * @return byte[]
+     * @throws Exception
      */
-    public synchronized BigInteger getPublicK() {
-        return publicK;
-    }
+    public static byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
 
-    /**
-     * Return the private key.
-     */
-    public synchronized BigInteger getPrivateK() {
-        return privateK;
+        return cipher.doFinal(encrypted);
     }
 }
